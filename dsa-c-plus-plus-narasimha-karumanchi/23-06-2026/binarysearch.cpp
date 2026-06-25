4396f569ed2cf6e89527219d6283415eabc010b9
Adeel Arif <adeelarifknp@gmail.com>
2026-06-25 22:29:56 +0530

    some fixes in bubblesort and binary search code

--- dsa-c-plus-plus-narasimha-karumanchi/23-06-2026/binarysearch.cpp
+++ dsa-c-plus-plus-narasimha-karumanchi/23-06-2026/binarysearch.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int* a, int len);
+int binarySearch(int* a,int si,int li,int key);
+
+int main(){
+	int arr[] = {2,10,15,21,23,32,39,41,46};
+	int key = 41;
+	int len=sizeof(arr)/sizeof(arr[0]);
+	printArray(arr,len);
+	int resultIndex = binarySearch(arr,0,len-1,key);
+	if(resultIndex != -1){
+		cout << "Element : " << key << "\t" <<  "Found at index: " << resultIndex << "\n";
+	} else {
+		cout << "Element : " << key << "\t" << "Not found in the array. \n";
+	}
+	return 0;
+}
+
+
+void printArray(int* a, int len){
+	for(int i=0;i<len;i++){
+		cout << a[i] << "\t";
+	}
+	cout << "\n";
+}
+
 int binarySearch(int* a, int si, int li, int key){
 	if( si <= li){
 		int mid = (si + li)/2;
@@ -13,14 +38,5 @@ int binarySearch(int* a, int si, int li, int key){
 		}
 	} 
 	return -1;
-}
-
-int main(){
-	int arr[] = {2,10,15,21,23,32,39,41,46};
-	int key = 41;
-	int 
-	binarySearch();
-	cout << "element at index " << key << "\n";
-	return 0;
 }
 
--- dsa-c-plus-plus-narasimha-karumanchi/23-06-2026/bubblesort.cpp
+++ dsa-c-plus-plus-narasimha-karumanchi/23-06-2026/bubblesort.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int* a, int len);
+void bubbleSort(int* a,int len);
+
+int main(){
+	int arr[] = {7,3,8,9,5};
+	int len = sizeof(arr)/sizeof(arr[0]);
+	printArray(arr,len);
+	bubbleSort(arr,len);
+	printArray(arr,len);
+	return 0;
+}
+
+void printArray(int* a,int len){
+	for(int i=0; i<len ; i++){
+		cout << a[i] << "\t";
+	}
+	cout << "\n";
+}
+
+
 void bubbleSort(int* a,int len){
 	int temp;
 	int flag;
@@ -18,14 +38,4 @@ void bubbleSort(int* a,int len){
 	}
 }
 
-int main(){
-	int arr[] = {7,3,8,9,5};
-	int len = sizeof(arr)/sizeof(arr[0]);
-	bubbleSort(arr,len);
-	int k;
-	for(int k=0; k<len ; k++){
-		cout <<	arr[k] << " " << endl;
-	}
-	return 0;
-}
 
